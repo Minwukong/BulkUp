a793e189b78a021c06afb8b8657747fc337d4c68
Minwukong <jh02035@gmail.com>
2021-05-16 21:35:54 +0900

    no message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,25 @@ import React from 'react';
 import './App.css';
 import MainPage from './pages/MainPage';
 function App() {
-    return <MainPage></MainPage>;
+    const isLogin = false;
+    <div>
+      <Header />
+      {isLogin ? (
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route path="/mypage" component={MyPage} />
+          <Route path="/404" component={NotFound} />
+          <Redirect from="*" to="/404" />
+        </Switch>
+      ) : (
+        <Switch>
+          <Route exact path="/" component={Login} />}
+          <Route path="/signup" component={SignUp} />
+          <Route path="/404" component={NotFound} />
+          <Redirect from="*" to="/404" />
+        </Switch>
+      )}
+    </div>
 }
 
 export default App;
